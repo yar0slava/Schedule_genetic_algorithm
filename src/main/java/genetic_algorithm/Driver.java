ab352619b81b3de2fe02b21a24e46421cd610677
yar0slava <yavacollins@gmail.com>
2020-10-18 18:41:33 -0700

    minor changes

--- src/main/java/genetic_algorithm/Driver.java
+++ src/main/java/genetic_algorithm/Driver.java
@@ -18,38 +18,38 @@ public class Driver {
         driver.data = new InitialData();
         driver.printAvailableData();
 
-        System.out.println(" -- Generation Number: "+ generationNumber);
-        System.out.printf("%1s %7s %11s %27s"," #", "Health", "Conflicts", "  Classes [Class -- ClassTime -- Group -- ClassTime -- Lecturer] " + "\n");
+        System.out.println(" -- Generation Number: " + generationNumber);
+        System.out.printf("%1s %7s %11s %27s", " #", "Health", "Conflicts", "  Classes [Class -- ClassTime -- Group -- ClassTime -- Lecturer] " + "\n");
         GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm(driver.data);
         Population population = new Population(Driver.POPULATION_SIZE, driver.data).sortByHealth();
-        for (ScheduleIndividual schedule: population.getScheduleIndividuals()) {
-            System.out.printf("%2s %4s %9s  %12s",driver.scheduleNumb++ + " |",
-                    String.format("%.5f",schedule.getHealth()) + " |",schedule.getConflicts() + "  |", schedule);
+        for (ScheduleIndividual schedule : population.getScheduleIndividuals()) {
+            System.out.printf("%2s %4s %9s  %12s", driver.scheduleNumb++ + " |",
+                    String.format("%.5f", schedule.getHealth()) + " |", schedule.getConflicts() + "  |", schedule);
             System.out.println();
 
         }
         driver.classNumb = 1;
 
         while (population.getScheduleIndividuals().get(0).getHealth() != 1.0) {
-            System.out.println(" -- Generation Number: "+ ++generationNumber);
-            System.out.printf("%1s %7s %11s %27s"," #", "Health", "Conflicts", "  Classes [Class -- ClassTime -- Group -- ClassTime -- Lecturer] " + "\n");
+            System.out.println(" -- Generation Number: " + ++generationNumber);
+            System.out.printf("%1s %7s %11s %27s", " #", "Health", "Conflicts", "  Classes [Class -- ClassTime -- Group -- ClassTime -- Lecturer] " + "\n");
             population = geneticAlgorithm.generateEvolution(population).sortByHealth();
             driver.scheduleNumb = 0;
-            for (ScheduleIndividual schedule: population.getScheduleIndividuals()) {
-                System.out.printf("%2s %4s %9s  %12s",driver.scheduleNumb++ + " |",
-                        String.format("%.5f",schedule.getHealth()) + " |",schedule.getConflicts() + "  |", schedule + "\n");
+            for (ScheduleIndividual schedule : population.getScheduleIndividuals()) {
+                System.out.printf("%2s %4s %9s  %12s", driver.scheduleNumb++ + " |",
+                        String.format("%.5f", schedule.getHealth()) + " |", schedule.getConflicts() + "  |", schedule + "\n");
             }
             System.out.println();
 
             driver.classNumb = 1;
         }
-        if(population.getScheduleIndividuals().get(0).getHealth() == 1.0){
-                driver.printScheduleAsTable(population.getScheduleIndividuals().get(0), generationNumber);
-            }
+        if (population.getScheduleIndividuals().get(0).getHealth() == 1.0) {
+            driver.printScheduleAsTable(population.getScheduleIndividuals().get(0), generationNumber);
+        }
 
     }
 
-    private void printScheduleAsTable(ScheduleIndividual schedule, int generation){
+    private void printScheduleAsTable(ScheduleIndividual schedule, int generation) {
         ArrayList<Class> classes = schedule.getClasses();
 
         System.out.print("\n                       ");
@@ -58,7 +58,7 @@ public class Driver {
         System.out.print("------------------------------------------------------");
         System.out.println("---------------------------------------------------------------");
 
-        for (Class classees: schedule.getClasses()) {
+        for (Class classees : schedule.getClasses()) {
             System.out.print("                       ");
             System.out.print(String.format("  %1$02d  ", classNumb) + "  | ");
             System.out.printf("%1$21s", classees.getDiscipline() + ")|");
@@ -70,7 +70,7 @@ public class Driver {
 
             classNumb++;
         }
-        if (schedule.getHealth() == 1) System.out.println("> Solution Found in "+ (generation + 1) +" generations");
+        if (schedule.getHealth() == 1) System.out.println("> Solution Found in " + (generation + 1) + " generations");
         System.out.print("-----------------------------------------------------------------------------------");
         System.out.println("-------------------------------------------------------------------------------------");
     }
@@ -78,11 +78,11 @@ public class Driver {
     private void printAvailableData() {
 
         System.out.println(" ....... Group ....... ");
-       System.out.println("+---------+---------+---------+---------------+");
+        System.out.println("+---------+---------+---------+---------------+");
         System.out.printf("%9s %9s %9s %12s", "Group ID", "Name", "Amount", "Lecturer" + "\n");
         System.out.println("+---------+---------+---------+---------------+");
         for (Group group : data.getGroups()) {
-            System.out.format("%9s %8s %8s %18s",group.getId(), group.getName(), group.getAmount(),group.getLecturer() + "\n");
+            System.out.format("%9s %8s %8s %18s", group.getId(), group.getName(), group.getAmount(), group.getLecturer() + "\n");
         }
 
 
@@ -92,7 +92,7 @@ public class Driver {
         System.out.println("+------+---------+----------------------------------");
         for (Discipline discipline : data.getDisciplines()) {
 
-            System.out.format("%3s %9s %38s",discipline.getId(), discipline.getName(), discipline.getGroups() + "\n");
+            System.out.format("%3s %9s %38s", discipline.getId(), discipline.getName(), discipline.getGroups() + "\n");
         }
 
         System.out.println("\n .. Classroom .. ");
@@ -100,7 +100,7 @@ public class Driver {
         System.out.printf("%9s %11s", "Room_ID", "Capacity" + "\n");
         System.out.println("+---------+---------+");
         for (Classroom classrooms : data.getClassrooms()) {
-            System.out.format("%6s %11s",classrooms.getId(),classrooms.getCapacity() + "\n");
+            System.out.format("%6s %11s", classrooms.getId(), classrooms.getCapacity() + "\n");
         }
 
 
@@ -109,7 +109,7 @@ public class Driver {
         System.out.printf("%6s %15s", "ID", "Name" + "\n");
         System.out.println("+-------+-------------------+");
         for (Lecturer lecturer : data.getLecturers()) {
-            System.out.format("%6s %20s",lecturer.getId(), lecturer.getName() + "\n");
+            System.out.format("%6s %20s", lecturer.getId(), lecturer.getName() + "\n");
         }
 
         System.out.println("\n ..... ClassTime .....");
@@ -117,7 +117,7 @@ public class Driver {
         System.out.printf("%6s %15s", "ID", "Name" + "\n");
         System.out.println("+-------+-------------------+");
         for (ClassTime time : data.getClassTimes()) {
-            System.out.format("%6s %20s",time.getId(),time.getTime() + "\n");
+            System.out.format("%6s %20s", time.getId(), time.getTime() + "\n");
         }
         System.out.println();
 
